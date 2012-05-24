7ce38263ce8686b6945226c445f0adadc6375101
Sandy Ruby <ray-nger@yandex.ru>
2012-05-24 22:36:33 +0300

    bpages 1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BizpagesOrg::Application.config.secret_token = '65c00e9a42ce69ae7697d9461ddc595781fd5dceb032f5f99abff4b7ecbbd999ba7c87ddb93d5c34a120d45d6d6d0152c1c23a3bc717e005b880dd0bfca8039d'
